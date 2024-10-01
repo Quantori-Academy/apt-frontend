257f35e29c0839fef436e0e5924a5eff9b3647eb
kazrias <ka6rias@gmail.com>
2024-10-01 19:56:45 +0400

    Add Login page to routerConfig

--- src/router/routerConfig.tsx
+++ src/router/routerConfig.tsx
@@ -1,14 +1,16 @@
 import { RouteProps } from "react-router-dom";
 
-import { Home } from "@/pages";
+import { Home, Login } from "@/pages";
 
 export const enum AppRoutes {
   HOME = "home",
+  LOGIN = "login",
   NOT_FOUND = "not_found",
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
   [AppRoutes.HOME]: "/",
+  [AppRoutes.LOGIN]: "/login",
   [AppRoutes.NOT_FOUND]: "*",
 };
 
@@ -16,6 +18,10 @@ export const routerConfig: Record<AppRoutes, RouteProps> = {
   [AppRoutes.HOME]: {
     path: RoutePath[AppRoutes.HOME],
     element: <Home />,
+  },
+  [AppRoutes.LOGIN]: {
+    path: RoutePath[AppRoutes.LOGIN],
+    element: <Login />,
   },
   [AppRoutes.NOT_FOUND]: {
     path: RoutePath[AppRoutes.NOT_FOUND],
