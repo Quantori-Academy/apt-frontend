356000d8e8a1017f577c4ddbac1da167209193ed
Anahit Margaryan <anahits.codes@gmail.com>
2024-10-02 22:42:17 +0400

    change name user to userList in router config

--- src/router/routerConfig.tsx
+++ src/router/routerConfig.tsx
@@ -1,17 +1,21 @@
 import { RouteProps } from "react-router-dom";
 
-import { Home, Login } from "@/pages";
+import { AddUserForm, Home, UserList, Login } from "@/pages";
 
 export const enum AppRoutes {
   HOME = "home",
   LOGIN = "login",
   NOT_FOUND = "not_found",
+  ADD_USER = "add_user",
+  USERS = "users",
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
   [AppRoutes.HOME]: "/",
   [AppRoutes.LOGIN]: "/login",
   [AppRoutes.NOT_FOUND]: "*",
+  [AppRoutes.USERS]: "/users",
+  [AppRoutes.ADD_USER]: "/users/new",
 };
 
 export const routerConfig: Record<AppRoutes, RouteProps> = {
@@ -22,6 +26,14 @@ export const routerConfig: Record<AppRoutes, RouteProps> = {
   [AppRoutes.LOGIN]: {
     path: RoutePath[AppRoutes.LOGIN],
     element: <Login />,
+  },
+  [AppRoutes.ADD_USER]: {
+    path: RoutePath[AppRoutes.ADD_USER],
+    element: <AddUserForm />,
+  },
+  [AppRoutes.USERS]: {
+    path: RoutePath[AppRoutes.USERS],
+    element: <UserList />,
   },
   [AppRoutes.NOT_FOUND]: {
     path: RoutePath[AppRoutes.NOT_FOUND],
