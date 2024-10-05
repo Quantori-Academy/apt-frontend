eec5dd6a282843791bf5fd07617b73185d1f6a74
kazrias <ka6rias@gmail.com>
2024-10-06 01:14:27 +0400

    Create table component, Add static users list for testing

--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -1,85 +1,23 @@
-import {
-  Button,
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-} from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 
-import { UserTableColumn } from "@/types";
+import { UsersTable } from "@/components";
 
-const users = [
-  {
-    id: 1,
-    username: "john_doe",
-    firstName: "John",
-    lastName: "Doe",
-    email: "john@example.com",
-    role: "Admin",
-    lastLogin: "2024-10-01",
-  },
-  {
-    id: 2,
-    username: "jane_smith",
-    firstName: "Jane",
-    lastName: "Smith",
-    email: "jane@example.com",
-    role: "User",
-    lastLogin: "2024-09-29",
-  },
-];
-const columns: UserTableColumn[] = [
-  { label: "Username", key: "username" },
-  { label: "First Name", key: "firstName" },
-  { label: "Last Name", key: "lastName" },
-  { label: "Email", key: "email" },
-  { label: "Role", key: "role" },
-  { label: "Last Login", key: "lastLogin" },
-  { label: "Actions", key: "actions" },
-];
 const Users: React.FC = () => {
-  const handleEdit = (userId: number) => {
-    console.log(`Edit user with id: ${userId}`);
-  };
   return (
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            {columns.map((column) => (
-              <TableCell key={column.key}>{column.label}</TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {users.map((user) => (
-            <TableRow key={user.id}>
-              {columns.map((column) => {
-                if (column.key !== "actions") {
-                  return (
-                    <TableCell key={column.key}>
-                      {user[column.key as keyof typeof user]}
-                    </TableCell>
-                  );
-                }
-              })}
-              <TableCell>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={() => handleEdit(user.id)}
-                >
-                  Edit
-                </Button>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Container>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          marginBottom: "20px",
+        }}
+      >
+        <Typography variant="h2">Users list</Typography>
+        <Button variant="outlined">Add User</Button>
+      </Box>
+      <UsersTable />
+    </Container>
   );
 };
 
