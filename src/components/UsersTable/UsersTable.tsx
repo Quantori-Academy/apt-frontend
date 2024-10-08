05206d30c9ff76b64c5d8c66c9028fd60703f6a1
kazrias <ka6rias@gmail.com>
2024-10-08 22:30:56 +0400

    Delete static data

--- src/components/UsersTable/UsersTable.tsx
+++ src/components/UsersTable/UsersTable.tsx
@@ -11,88 +11,6 @@ import {
 
 import { UserTableColumn, UserTableProps } from "@/types";
 
-// const staticUsers = [
-//   {
-//     id: 1,
-//     username: "tammyfernandez",
-//     firstName: "Paige",
-//     lastName: "Avila",
-//     email: "chandlerchristopher@gmail.com",
-//     role: "Admin",
-//   },
-//   {
-//     id: 2,
-//     username: "caroline74",
-//     firstName: "Kathleen",
-//     lastName: "Hanna",
-//     email: "joshua84@hotmail.com",
-//     role: "Admin",
-//   },
-//   {
-//     id: 3,
-//     username: "glensingh",
-//     firstName: "Jesus",
-//     lastName: "Sanders",
-//     email: "charlotte45@gmail.com",
-//     role: "Researcher",
-//   },
-//   {
-//     id: 4,
-//     username: "debraharding",
-//     firstName: "Scott",
-//     lastName: "Simmons",
-//     email: "hhayes@brooks-hall.com",
-//     role: "Admin",
-//   },
-//   {
-//     id: 5,
-//     username: "shawnjones",
-//     firstName: "Felicia",
-//     lastName: "Ortiz",
-//     email: "tanyakemp@bruce-martinez.com",
-//     role: "Procurement Officer",
-//   },
-//   {
-//     id: 6,
-//     username: "wzavala",
-//     firstName: "Beth",
-//     lastName: "Kelley",
-//     email: "beth_kelley@example.com",
-//     role: "Researcher",
-//   },
-//   {
-//     id: 7,
-//     username: "marcus.jones",
-//     firstName: "Marcus",
-//     lastName: "Jones",
-//     email: "marcus.jones@example.com",
-//     role: "Admin",
-//   },
-//   {
-//     id: 8,
-//     username: "david_williams",
-//     firstName: "David",
-//     lastName: "Williams",
-//     email: "david.williams@example.com",
-//     role: "Procurement Officer",
-//   },
-//   {
-//     id: 9,
-//     username: "linda_smith",
-//     firstName: "Linda",
-//     lastName: "Smith",
-//     email: "linda.smith@example.com",
-//     role: "Researcher",
-//   },
-//   {
-//     id: 10,
-//     username: "roberthughes",
-//     firstName: "Robert",
-//     lastName: "Hughes",
-//     email: "roberthughes@example.com",
-//     role: "Admin",
-//   },
-// ];
 const columns: UserTableColumn[] = [
   { label: "Username", key: "username" },
   { label: "First Name", key: "first_name" },
@@ -118,7 +36,8 @@ const UsersTable: React.FC<UserTableProps> = ({ users }) => {
         </TableHead>
         <TableBody>
           {users.map((user) => (
-            <TableRow key={user.id}>
+            <TableRow key={user.email}>
+              {/* Change to user.id once backend sends id */}
               {columns.map((column) => {
                 if (column.key !== "actions") {
                   return (
