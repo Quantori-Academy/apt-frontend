b4a1c1701506ecd8b226e27eaf736e81953520c3
Avtex19 <atukaberadze19@gmail.com>
2024-10-10 18:03:08 +0400

    Add storage location management methods to ApiManager
    - Implement `getStorageLocations`, `addStorageLocation`, `editStorageLocation`, and `deleteStorageLocation` methods in ApiManager.
    - Each method retrieves or manipulates storage location data, with authentication using a token stored in localStorage.
    - Update HTTP methods with proper headers and body formatting for POST and PUT requests.
    -Implement corresponding endpoints

--- src/api/apiManager.ts
+++ src/api/apiManager.ts
@@ -1,5 +1,6 @@
 import { ApiStatus, Tokens } from "@/types";
 import { UserInputData } from "@/types";
+import { StorageLocation } from "@/types";
 
 import { apiMethods } from "./apiMethods";
 import { EndPoints } from "./endPoints";
@@ -11,6 +12,35 @@ class ApiManager {
   public login(credentials: UserInputData) {
     return apiMethods.post<Tokens>(EndPoints.LOGIN, {
       body: JSON.stringify(credentials),
+    });
+  }
+  public getStorageLocations() {
+    const token = localStorage.getItem("authToken");
+    return apiMethods.get<StorageLocation[]>(EndPoints.STORAGE_LOCATIONS, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+  }
+
+  public addStorageLocation(data: { room: string; name: string }) {
+    const token = localStorage.getItem("authToken");
+    return apiMethods.post<StorageLocation>(EndPoints.STORAGE_LOCATIONS, {
+      body: JSON.stringify(data),
+      headers: { Authorization: `Bearer ${token}`, "Content-Type": "application/json" },
+    });
+  }
+
+  public editStorageLocation(id: number, data: { room: string; name: string }) {
+    const token = localStorage.getItem("authToken");
+    return apiMethods.put<StorageLocation>(`${EndPoints.STORAGE_LOCATIONS}/${id}`, {
+      body: JSON.stringify(data),
+      headers: { Authorization: `Bearer ${token}`, "Content-Type": "application/json" },
+    });
+  }
+
+  public deleteStorageLocation(id: number) {
+    const token = localStorage.getItem("authToken");
+    return apiMethods.delete(`${EndPoints.STORAGE_LOCATIONS}/${id}`, {
+      headers: { Authorization: `Bearer ${token}` },
     });
   }
 }
