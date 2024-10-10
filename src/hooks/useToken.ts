0877a3096f9dbaceb3602f7d6c4fc91b05672b76
kazrias <ka6rias@gmail.com>
2024-10-11 01:36:46 +0400

    Fix useTocen hook

--- src/hooks/useToken.ts
+++ src/hooks/useToken.ts
@@ -6,11 +6,9 @@ import { useAppDispatch } from "./useAppDispatch";
 
 export const useToken = () => {
   const dispatch = useAppDispatch();
-  console.log("worked useToken");
   useEffect(() => {
     const token = localStorage.getItem("accessToken");
     if (token) {
-      console.log("worked if inside useToken");
       dispatch(setUser({ token }));
     }
   }, [dispatch]);
