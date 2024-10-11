53ab9aacc48de946b7b77640962745d598ae8531
kazrias <ka6rias@gmail.com>
2024-10-11 21:19:32 +0400

    Fix page refreshing

--- src/components/PageLayout/PageLayout.tsx
+++ src/components/PageLayout/PageLayout.tsx
@@ -4,11 +4,17 @@ import type { PropsWithChildren } from "react";
 import { useToken } from "@/hooks";
 
 import { Header } from "../Header";
+import { LoadingPage } from "../LoadingPage";
 
 import styles from "./index.module.css";
 
 const PageLayout: React.FC<PropsWithChildren> = ({ children }) => {
-  useToken();
+  const isLoading = useToken();
+
+  if (isLoading) {
+    return <LoadingPage />;
+  }
+
   return (
     <>
       <Header />
--- src/hooks/useToken.ts
+++ src/hooks/useToken.ts
@@ -1,15 +1,19 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { setUser } from "@/store/slices/authSlice";
 
 import { useAppDispatch } from "./useAppDispatch";
 
 export const useToken = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
   const dispatch = useAppDispatch();
   useEffect(() => {
     const token = localStorage.getItem("accessToken");
     if (token) {
       dispatch(setUser({ token }));
     }
+    setIsLoading(false);
   }, [dispatch]);
+  return isLoading;
 };
