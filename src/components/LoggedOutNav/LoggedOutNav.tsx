4d9b709721c3611ce406507441881deaaedf0603
kazrias <ka6rias@gmail.com>
2024-10-14 02:32:22 +0400

    Add LoggedOutNav component

---
+++ src/components/LoggedOutNav/LoggedOutNav.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
+const LoggedOutNav: React.FC = () => (
+  <Button component={NavLink} to="/login">
+    Login
+  </Button>
+);
+export default LoggedOutNav;
---
+++ src/components/LoggedOutNav/index.ts
@@ -0,0 +1,1 @@
+export { default as LoggedOutNav } from "./LoggedOutNav.tsx";
