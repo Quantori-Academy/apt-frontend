b0c6af0f4b2fcea4a5d0e0cd0d00b9b76336c40f
kazrias <ka6rias@gmail.com>
2024-10-15 00:24:39 +0400

    Refactor AppRouter to use RouterProvider and createBrowserRouter - Replaced Routes and Route components with RouterProvider and createBrowserRouter - Moved public and protected routes into `routes` array. - Wrapped routes with PageLayout as a common layout component.

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,32 +1,31 @@
-import { Suspense } from "react";
-import { Route, Routes } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import { PageLoader } from "@/components";
+import { PageLayout, PageLoader } from "@/components";
 
 import { ProtectedRoute } from "./ProtectedRoute";
 import { protectedRoutesRouterConfig } from "./protectedRoutesRouterConfig";
 import { publicRoutesRouterConfig } from "./publicRoutesRouterConfig";
 
-const AppRouter = () => {
-  return (
-    <Suspense fallback={<PageLoader />}>
-      <Routes>
-        {Object.values(publicRoutesRouterConfig).map(({ path, element }) => (
-          <Route key={path} path={path} element={element} />
-        ))}
+const routes = [
+  {
+    element: <PageLayout />,
+    children: [
+      ...Object.values(publicRoutesRouterConfig),
 
-        {Object.values(protectedRoutesRouterConfig).map(
-          ({ path, element, roles }) => (
-            <Route
-              key={path}
-              path={path}
-              element={<ProtectedRoute element={element} roles={roles} />}
-            />
-          )
-        )}
-      </Routes>
-    </Suspense>
-  );
-};
+      ...Object.values(protectedRoutesRouterConfig).map(
+        ({ path, element, roles }) => ({
+          path,
+          element: <ProtectedRoute element={element} roles={roles} />,
+        })
+      ),
+    ],
+  },
+];
+
+const router = createBrowserRouter(routes);
+
+const AppRouter = () => (
+  <RouterProvider router={router} fallbackElement={<PageLoader />} />
+);
 
 export default AppRouter;
