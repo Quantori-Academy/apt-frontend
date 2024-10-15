1d992b014edd82982c50a1265710ff959366e60b
DmitriyMischenko-Quantori <dmitriy.mischenko@quantori.com>
2024-10-16 00:40:02 +0400

    setup testing env

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
@@ -8,5 +9,16 @@ export default defineConfig({
 
   server: {
     port: 3000,
+  },
+
+  test: {
+    globals: true,
+    passWithNoTests: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+    coverage: {
+      all: false,
+      enabled: true,
+    },
   },
 });
