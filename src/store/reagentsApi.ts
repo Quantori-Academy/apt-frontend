23ec6e6a26c5de34a40fb80ed1c8f174af1fb36a
kazrias <ka6rias@gmail.com>
2024-10-17 20:19:57 +0400

    Add GET (mockData) and DELETE requests in reagentApi

--- src/store/reagentsApi.ts
+++ src/store/reagentsApi.ts
@@ -1,3 +1,4 @@
+import { reagentDetails } from "@/mockData/reagentDetails";
 import { Reagent } from "@/types";
 
 import { baseApi } from "./baseApi";
@@ -5,10 +6,24 @@ import { baseApi } from "./baseApi";
 export const reagentsApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getReagentDetails: builder.query<Reagent, string>({
-      query: (reagentId) => `/reagents/${reagentId}`,
+      // query: (reagentId) => `/reagents/${reagentId}`,
+      queryFn: async () => {
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            resolve({ data: reagentDetails });
+          }, 100);
+        });
+      },
       providesTags: ["Reagents"],
+    }),
+    deleteReagent: builder.mutation({
+      query: (reagentId) => ({
+        url: `reagents/${reagentId}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Reagents"],
     }),
   }),
   overrideExisting: true,
 });
-export const { useGetReagentDetailsQuery } = reagentsApi;
+export const { useGetReagentDetailsQuery, useDeleteReagentMutation } = reagentsApi;
