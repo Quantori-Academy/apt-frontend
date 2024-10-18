e478e7e8f007ada0c9db24425ddf770a835ef277
kazrias <ka6rias@gmail.com>
2024-10-18 12:46:45 +0400

    Fix public and protected routes

--- src/router/protectedRoutesRouterConfig.tsx
+++ src/router/protectedRoutesRouterConfig.tsx
@@ -1,7 +1,13 @@
 import { RouteObject } from "react-router-dom";
 
 import { userRoles } from "@/constants";
-import { AccountSettings, Dashboard, Users } from "@/pages";
+import {
+  AccountSettings,
+  Dashboard,
+  StorageLocationDetailPage,
+  StorageLocationPage,
+  Users,
+} from "@/pages";
 import { UserRole } from "@/types";
 
 type AppRoutesProps = RouteObject & {
@@ -12,6 +18,8 @@ export const enum AppProtectedRoutes {
   USERS = "users",
   USER_PAGE = "user_page",
   ACCOUNT_SETTINGS = "account_settings",
+  STORAGE_LOCATIONS = "storage",
+  STORAGE_LOCATION_DETAIL = "storage_detail",
   DASHBOARD = "dashboard",
 }
 
@@ -19,6 +27,8 @@ export const RouteProtectedPath: Record<AppProtectedRoutes, string> = {
   [AppProtectedRoutes.USERS]: "/users",
   [AppProtectedRoutes.USER_PAGE]: "/users/:id",
   [AppProtectedRoutes.ACCOUNT_SETTINGS]: "/account-settings",
+  [AppProtectedRoutes.STORAGE_LOCATIONS]: "/storage",
+  [AppProtectedRoutes.STORAGE_LOCATION_DETAIL]: "/storage/:locationId",
   [AppProtectedRoutes.DASHBOARD]: "/dashboard",
 };
 
@@ -35,6 +45,16 @@ export const protectedRoutesRouterConfig: Record<
     path: RouteProtectedPath[AppProtectedRoutes.USER_PAGE],
     element: <AccountSettings />,
     roles: [userRoles.Administrator],
+  },
+  [AppProtectedRoutes.STORAGE_LOCATIONS]: {
+    path: RouteProtectedPath[AppProtectedRoutes.STORAGE_LOCATIONS],
+    element: <StorageLocationPage />,
+    roles: Object.values(userRoles),
+  },
+  [AppProtectedRoutes.STORAGE_LOCATION_DETAIL]: {
+    path: RouteProtectedPath[AppProtectedRoutes.STORAGE_LOCATION_DETAIL],
+    element: <StorageLocationDetailPage />,
+    roles: Object.values(userRoles),
   },
   [AppProtectedRoutes.ACCOUNT_SETTINGS]: {
     path: RouteProtectedPath[AppProtectedRoutes.ACCOUNT_SETTINGS],
--- src/router/publicRoutesRouterConfig.tsx
+++ src/router/publicRoutesRouterConfig.tsx
@@ -1,24 +1,19 @@
 import { RouteObject } from "react-router-dom";
 
 import { Home, Login } from "@/pages";
-import { StorageLocationDetailPage } from "@/pages/StorageLocationDetail";
-import { StorageLocationPage } from "@/pages/StorageLocations";
 
 import { loginLoader } from "./utils/loginLoader";
 
 export const enum AppPublicRoutes {
   HOME = "home",
   LOGIN = "login",
-  STORAGE_LOCATIONS = "storage",
-  STORAGE_LOCATION_DETAIL = "storage_detail",
+
   NOT_FOUND = "not_found",
 }
 
 export const RoutePublicPath: Record<AppPublicRoutes, string> = {
   [AppPublicRoutes.HOME]: "/",
   [AppPublicRoutes.LOGIN]: "/login",
-  [AppPublicRoutes.STORAGE_LOCATIONS]: "/storage",
-  [AppPublicRoutes.STORAGE_LOCATION_DETAIL]: "/storage/:locationId",
 
   [AppPublicRoutes.NOT_FOUND]: "*",
 };
@@ -32,14 +27,6 @@ export const publicRoutesRouterConfig: Record<AppPublicRoutes, RouteObject> = {
     path: RoutePublicPath[AppPublicRoutes.LOGIN],
     element: <Login />,
     loader: loginLoader,
-  },
-  [AppPublicRoutes.STORAGE_LOCATIONS]: {
-    path: RoutePublicPath[AppPublicRoutes.STORAGE_LOCATIONS],
-    element: <StorageLocationPage />,
-  },
-  [AppPublicRoutes.STORAGE_LOCATION_DETAIL]: {
-    path: RoutePublicPath[AppPublicRoutes.STORAGE_LOCATION_DETAIL],
-    element: <StorageLocationDetailPage />,
   },
   [AppPublicRoutes.NOT_FOUND]: {
     path: RoutePublicPath[AppPublicRoutes.NOT_FOUND],
