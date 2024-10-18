954fb3f3c581780f0c3c45295eee732f56ed1dc3
kazrias <ka6rias@gmail.com>
2024-10-18 12:46:17 +0400

    Fix imports

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,3 +3,5 @@ export { Users } from "./Users";
 export { AccountSettings } from "./AccountSettings";
 export { Dashboard } from "./Dashboard";
 export { Home } from "./Home";
+export { StorageLocationPage } from "./StorageLocations";
+export { StorageLocationDetailPage } from "./StorageLocationDetail";
