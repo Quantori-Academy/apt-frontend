1e631beca0b922adfd708377808d955fd5159636
kazrias <ka6rias@gmail.com>
2024-10-21 02:05:33 +0400

    Add custom hook, submit function, quantity field,  improve autocomplete

--- src/components/ReagentEditForm/ReagentEditForm.tsx
+++ src/components/ReagentEditForm/ReagentEditForm.tsx
@@ -7,141 +7,129 @@ import {
   DialogTitle,
   TextField,
 } from "@mui/material";
-import { useEffect, useState } from "react";
 
 import { PageLoader } from "@/components";
-import { useGetStorageRoomsQuery } from "@/store";
-import { Reagent, RoomLocationBrief, StorageRoomsBrief } from "@/types";
-
-// const roomsData = [
-//   {
-//     roomId: "1",
-//     roomName: "Room 101",
-//     locationsInRoom: [
-//       {
-//         locationId: "3",
-//         locationName: "shelf 3",
-//         reagentsNumberInLocation: 9,
-//       },
-//     ],
-//   },
-//   {
-//     roomId: "2",
-//     roomName: "Room 102",
-//     locationsInRoom: [
-//       {
-//         locationId: "5",
-//         locationName: "shelf 13",
-//         reagentsNumberInLocation: 19,
-//       },
-//     ],
-//   },
-// ];
+import { useLocationQuantityDetails } from "@/hooks";
+import {
+  useDeleteReagentMutation,
+  useGetStorageRoomsQuery,
+  useUpdateReagentMutation,
+} from "@/store";
+import { FrontEndReagent, FrontRoomData } from "@/types";
 
 type ReagentEditFormProps = {
   isEditing: boolean;
   setIsEditing: React.Dispatch<React.SetStateAction<boolean>>;
-  reagentDetails: Reagent;
+  reagentDetails: FrontEndReagent;
+  reagentLocationDetails: FrontRoomData;
 };
 
 const ReagentEditForm: React.FC<ReagentEditFormProps> = ({
   isEditing,
   setIsEditing,
   reagentDetails,
+  reagentLocationDetails,
 }) => {
-  const [selectedRoom, setSelectedRoom] = useState<StorageRoomsBrief | null>(
-    null
-  );
-  const [selectedLocation, setSelectedLocation] =
-    useState<RoomLocationBrief | null>(null);
-
   const { data: rooms, isLoading } = useGetStorageRoomsQuery();
-  useEffect(() => {
-    if (reagentDetails && rooms?.length) {
-      const currentRoom = rooms.find(
-        (room) => room.id === reagentDetails.storageLocation.roomId
-      );
-      setSelectedRoom(currentRoom || null);
 
-      if (currentRoom) {
-        const currentLocation = currentRoom.locations.find(
-          (location) =>
-            location.location_id === reagentDetails.storageLocation.locationId
-        );
-        console.log(currentRoom, "Room");
-        console.log(currentLocation, "location");
-        setSelectedLocation(currentLocation || null);
-      }
-    }
-  }, [reagentDetails, rooms]);
+  const [updateReagent] = useUpdateReagentMutation();
+
+  const [deleteReagent] = useDeleteReagentMutation();
+
+  const {
+    selectedRoom,
+    setSelectedRoom,
+    selectedLocation,
+    setSelectedLocation,
+    quantityLeft,
+    setQuantityLeft,
+  } = useLocationQuantityDetails(reagentDetails, reagentLocationDetails, rooms);
 
   if (isLoading) {
     return <PageLoader />;
   }
-  const handleSubmit = () => {
-    console.log("submited");
+
+  const handleSubmit = async () => {
+    try {
+      if (quantityLeft === "0") {
+        await deleteReagent(reagentDetails.substanceId).unwrap();
+      } else {
+        await updateReagent({
+          id: reagentDetails.substanceId,
+          quantity: quantityLeft,
+          storageLocation: {
+            roomId: selectedRoom?.id,
+            locationId: selectedLocation?.locationId,
+          },
+        }).unwrap();
+      }
+      setIsEditing(false);
+    } catch (err) {
+      console.error("Failed to update reagent: ", err);
+    }
   };
+
   const availableLocations = selectedRoom ? selectedRoom.locations : [];
+
   return (
-    <Dialog
-      open={isEditing}
-      onClose={() => setIsEditing(false)}
-      fullWidth
-      maxWidth="sm"
-    >
+    <Dialog open={isEditing} onClose={() => setIsEditing(false)} maxWidth="sm">
       <DialogTitle>Edit Reagent</DialogTitle>
       <DialogContent sx={{ paddingTop: "10px !important" }}>
-        {/* <TextField
+        <TextField
           label="Quantity Left"
           variant="outlined"
-          value={quantity}
-          onChange={(event) => setQuantity(event.target.value)}
+          value={quantityLeft}
+          inputProps={{ min: 0 }}
+          onChange={(event) => setQuantityLeft(event.target.value)}
           type="number"
           fullWidth
-          margin="normal"
-        /> */}
-
+          error={quantityLeft === "0"}
+          helperText={
+            quantityLeft === "0" && "Reagent will be deleted, if quantity is 0"
+          }
+          sx={{ marginBottom: "20px" }}
+        />
         <Autocomplete
-          sx={{ marginBottom: "25px" }}
+          sx={{ marginBottom: "20px" }}
           options={rooms || []}
-          getOptionLabel={(option) => option.room}
+          getOptionLabel={(option) =>
+            option.room +
+            (option.id === reagentLocationDetails.roomId ? " (current)" : "")
+          }
           value={selectedRoom}
-          onChange={(event, newRoom) => {
-            console.log(event);
+          onChange={(_, newRoom) => {
             setSelectedLocation(null);
             setSelectedRoom(newRoom);
           }}
           renderInput={(params) => (
-            <TextField {...params} label="Select Room" variant="outlined" />
+            <TextField {...params} label="Room" variant="outlined" />
           )}
         />
-
         {selectedRoom && (
           <Autocomplete
             sx={{ marginBottom: "25px" }}
             options={availableLocations}
-            getOptionLabel={(option) => option.location_name}
+            getOptionLabel={(option) =>
+              option.locationName +
+              (option.locationId === reagentLocationDetails.locationId
+                ? " (current)"
+                : "")
+            }
             value={selectedLocation}
-            onChange={(event, newLocation) => {
-              console.log(event);
+            onChange={(_, newLocation) => {
               setSelectedLocation(newLocation);
             }}
             renderInput={(params) => (
-              <TextField
-                {...params}
-                label="Select Location"
-                variant="outlined"
-              />
+              <TextField {...params} label="Location" variant="outlined" />
             )}
           />
         )}
-
         <Box sx={{ display: "flex", gap: "10px" }}>
           <Button
             variant="contained"
             color="primary"
             onClick={handleSubmit}
-            disabled={!selectedRoom || !selectedLocation}
+            disabled={!selectedRoom || !selectedLocation || quantityLeft === ""}
           >
             Save Changes
           </Button>
