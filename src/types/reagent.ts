1b723e8ff0a61de13e3da468f6cd65894697fa26
kazrias <ka6rias@gmail.com>
2024-10-21 02:14:41 +0400

    Add types

--- src/types/reagent.ts
+++ src/types/reagent.ts
@@ -1,22 +1,28 @@
-export type StorageLocation = {
-  roomId: string;
-  roomName: string;
-  locationId: string;
-  locationName: string;
-};
-
-export type Reagent = {
-  reagentID: string;
+export type FrontEndReagent = {
+  substanceId: number;
   name: string;
   category: string;
-  description: string;
-  structure: string;
   CASNumber: string;
   producer: string;
-  catalogID: string;
+  locationId: number;
+  unit: string;
+  pricePerUnit: string;
+  totalQuantityLeft: number;
+  catalogID: number;
   catalogLink: string;
-  storageLocation: StorageLocation;
-  units: string;
-  pricePerUnit: number;
-  quantity: number;
+  description: string;
+  structure: string;
+};
+
+export type BackEndReagent = Omit<
+  FrontEndReagent,
+  "substanceId" | "CASNumber" | "catalogID" | "catalogLink" | "totalQuantityLeft" | "pricePerUnit" | "locationId"
+> & {
+  substance_id: number;
+  cas_number: string;
+  catalog_id: number;
+  catalog_link: string;
+  total_quantity_left: number;
+  price_per_unit: string;
+  location_id: number;
 };
--- src/types/storageLocation.ts
+++ src/types/storageLocation.ts
@@ -1,12 +1,52 @@
-export type StorageRoomsBrief = {
-  id: string;
+export type FrontStorageRoomsBrief = {
+  id: number;
   room: string;
   description: string;
-  locations: RoomLocationBrief[];
+  locations: FrontRoomLocationBrief[];
+  totalSubstances: number;
+};
+
+export type FrontRoomLocationBrief = {
+  locationId: number;
+  locationName: string;
+};
+
+export type BackRoomLocationBrief = {
+  location_id: number;
+  location_name: string;
+};
+
+export type BackStorageRoomsBrief = Omit<FrontStorageRoomsBrief, "locations" | "totalSubstances"> & {
+  locations: BackRoomLocationBrief[];
   total_substances: number;
 };
 
-export type RoomLocationBrief = {
-  location_id: string;
+export type FrontRoomData = {
+  roomId: number;
+  locationId: number;
+  roomName: string;
+  locationName: string;
+  substances: FrontSubstance[];
+};
+
+export type FrontSubstance = {
+  substanceId: number;
+  name: string;
+  description: string;
+  structureSmiles: string;
+};
+
+export type BackRoomData = {
+  room_id: number;
+  location_id: number;
+  room_name: string;
   location_name: string;
+  substances: BackSubstance[];
+};
+
+export type BackSubstance = {
+  substance_id: number;
+  name: string;
+  description: string;
+  structure_smiles: string;
 };
