0ee3ac44072afe60bcabb481b8c49758b1c67aba
kazrias <ka6rias@gmail.com>
2024-10-21 18:31:58 +0400

    Add sampleApi

---
+++ src/store/sampleApi.ts
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+import { BASE_URL, prepareHeaders } from "@/api";
+import { BackEndSample, FrontEndSample } from "@/types/sample";
+
+import { transformSampleResponse } from "./utils/transformSampleResponse";
+
+export const sampleApi = createApi({
+  reducerPath: "sampleApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: BASE_URL,
+    prepareHeaders,
+  }),
+  tagTypes: ["Samples"],
+  endpoints: (builder) => ({
+    getSampleDetails: builder.query<FrontEndSample, string>({
+      query: (sampleId) => `/substances/samples/${sampleId}`,
+      transformResponse: (response: BackEndSample) => transformSampleResponse(response),
+      providesTags: ["Samples"],
+    }),
+  }),
+});
