1bd6d57a62c65e3780d25d2f565756482e8fc307
Anahit Margaryan <anahits.codes@gmail.com>
2024-10-21 13:02:50 +0400

    add sidebar component

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -1,19 +1,12 @@
-import DashboardIcon from "@mui/icons-material/Dashboard";
-import Inventory2Icon from "@mui/icons-material/Inventory2";
-import PeopleOutlineIcon from "@mui/icons-material/PeopleOutline";
-import ScienceIcon from "@mui/icons-material/Science";
-import { Box, Container, List, Paper } from "@mui/material";
+import { Box, Container, Paper } from "@mui/material";
 import * as React from "react";
 import { Outlet } from "react-router-dom";
 
 import { Header } from "@/components";
 import AccountMenu from "@/components/AccountMenu/AccountMenu.tsx";
+import { Sidebar } from "@/components/Sidebar";
 import { useAppSelector } from "@/hooks";
 import { selectUserIsAuthenticated } from "@/store";
-
-import { SidebarListItem } from "../SidebarListItem";
-
-import style from "./AppLayout.module.css";
 
 const AppLayout = () => {
   const isAuthenticated = useAppSelector(selectUserIsAuthenticated);
@@ -52,32 +45,10 @@ const AppLayout = () => {
         gridTemplateRows: "auto 1fr",
       }}
     >
-      {/* Header */}
       <Header onClick={handleClick} />
-
       <AccountMenu anchorEl={anchorEl} open={open} onClose={handleClose} />
 
-      {/* Sidebar */}
-
-      <Box className={style.sidebar}>
-        <List sx={{ position: "fixed" }}>
-          <SidebarListItem listItemText="Dashboard" to="/dashboard">
-            <DashboardIcon color="disabled" />
-          </SidebarListItem>
-          <SidebarListItem listItemText="Users" to="users">
-            <PeopleOutlineIcon color="disabled" />
-          </SidebarListItem>
-          <SidebarListItem listItemText="Storage Location" to="storage">
-            <Inventory2Icon color="disabled" />
-          </SidebarListItem>
-          <SidebarListItem
-            listItemText="Reagents and Samples"
-            to="reagent-sample-list"
-          >
-            <ScienceIcon color="disabled" />
-          </SidebarListItem>
-        </List>
-      </Box>
+      <Sidebar />
       <Container>
         <Paper
           sx={{
