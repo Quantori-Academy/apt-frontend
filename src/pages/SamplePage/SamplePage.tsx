ea6e8dc462f3b5911dc88d405d855c461ec08d82
kazrias <ka6rias@gmail.com>
2024-10-21 21:14:40 +0400

    Add details to SamplePage

--- src/pages/SamplePage/SamplePage.tsx
+++ src/pages/SamplePage/SamplePage.tsx
@@ -1,5 +1,43 @@
+import { Container } from "@mui/material";
+import { useParams } from "react-router-dom";
+
+import { PageLoader, SampleDetails } from "@/components";
+import { PageError } from "@/components/PageError";
+import {
+  useGetSampleDetailsQuery,
+  useGetStorageLocationDetailQuery,
+} from "@/store";
+
 const SamplePage = () => {
-  return <div>SamplePage</div>;
+  const { id: sampleId } = useParams();
+  const { data: sampleDetails, isLoading: isSampleLoading } =
+    useGetSampleDetailsQuery(sampleId!);
+
+  const { data: sampleLocationDetails, isLoading: isSampleLocationLoadiong } =
+    useGetStorageLocationDetailQuery(sampleDetails?.locationId as number, {
+      skip: !sampleDetails?.locationId,
+    });
+
+  if (isSampleLoading || isSampleLocationLoadiong) {
+    return <PageLoader />;
+  }
+
+  if (!sampleDetails || !sampleLocationDetails) {
+    return (
+      <PageError
+        text={"Failed to load sample details. Please try again later."}
+      />
+    );
+  }
+
+  return (
+    <Container maxWidth="md" sx={{ mt: 4 }}>
+      <SampleDetails
+        sampleDetails={sampleDetails}
+        sampleLocationDetails={sampleLocationDetails}
+      />
+    </Container>
+  );
 };
 
 export default SamplePage;
