6d10e145f7bfee14dead40f70875a446b6585887
Avtex19 <atukaberadze19@gmail.com>
2024-10-23 20:56:55 +0400

    fix: modify pages

--- src/pages/AddReagentPage/AddReagentPage.tsx
+++ src/pages/AddReagentPage/AddReagentPage.tsx
@@ -1,26 +1,11 @@
-import { Alert, Snackbar } from "@mui/material";
-import React, { useState } from "react";
+import React from "react";
 
 import { AddReagentForm } from "@/components/AddReagentForm";
+import useAlertSnackbar from "@/hooks/useAlertSnackbar";
 import { useCreateReagentMutation } from "@/store/reagentApi.ts";
 import { ReagentData } from "@/types/reagentData";
 
 const AddReagentPage: React.FC = () => {
-  const [reagentData, setReagentData] = useState<ReagentData>({
-    name: "",
-    description: "",
-    structure: "",
-    price_per_unit: -1,
-    quantity_unit: "",
-    quantity_left: -1,
-    expiration_date: new Date().toISOString().slice(0, 16),
-    location_id: 0,
-    cas_number: "",
-    producer: "",
-    catalog_id: 0,
-    catalog_link: "",
-  });
-
   const locationOptions = [
     { id: 1, label: "Location 1" },
     { id: 2, label: "Location 2" },
@@ -28,74 +13,25 @@ const AddReagentPage: React.FC = () => {
   ];
 
   const [createReagent] = useCreateReagentMutation();
+  const { openSnackbar, SnackbarComponent } = useAlertSnackbar();
 
-  const [openSnackbar, setOpenSnackbar] = useState(false);
-  const [snackbarMessage, setSnackbarMessage] = useState<string | null>(null);
-  const [severity, setSeverity] = useState<"success" | "error">("success");
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setReagentData({
-      ...reagentData,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleLocationChange = (
-    _event: React.ChangeEvent<unknown>,
-    value: { id: number; label: string } | null
-  ) => {
-    setReagentData((prevData) => ({
-      ...prevData,
-      location_id: value ? value.id : 0,
-    }));
-  };
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
+  const handleCreateReagent = async (reagentData: ReagentData) => {
     try {
       await createReagent(reagentData).unwrap();
-      setSnackbarMessage("Reagent added successfully!");
-      setSeverity("success");
-      setOpenSnackbar(true);
-
-      setTimeout(() => {
-        setOpenSnackbar(false);
-      }, 3000);
+      openSnackbar("success", "Reagent added successfully!");
     } catch (error) {
       console.error("Error adding reagent:", error);
-      setSnackbarMessage("Error adding reagent.");
-      setSeverity("error");
-      setOpenSnackbar(true);
+      openSnackbar("error", "Error adding reagent.");
     }
-  };
-
-  const handleCloseSnackbar = () => {
-    setOpenSnackbar(false);
   };
 
   return (
     <div>
       <AddReagentForm
-        reagentData={reagentData}
-        handleChange={handleChange}
-        handleSubmit={handleSubmit}
-        handleLocationChange={handleLocationChange}
+        handleCreateReagent={handleCreateReagent}
         locationOptions={locationOptions}
       />
-
-      <Snackbar
-        open={openSnackbar}
-        autoHideDuration={3000}
-        onClose={handleCloseSnackbar}
-      >
-        <Alert
-          onClose={handleCloseSnackbar}
-          severity={severity}
-          sx={{ width: "100%" }}
-        >
-          {snackbarMessage}
-        </Alert>
-      </Snackbar>
+      <SnackbarComponent />
     </div>
   );
 };
--- src/pages/AddSamplePage/AddSamplePage.tsx
+++ src/pages/AddSamplePage/AddSamplePage.tsx
@@ -1,23 +1,23 @@
-import { Alert, Snackbar } from "@mui/material";
-import React, { useState } from "react";
+import React from "react";
 
 import { AddSampleForm } from "@/components/AddSampleForm";
+import useAlertSnackbar from "@/hooks/useAlertSnackbar";
 import { useCreateSampleMutation } from "@/store/samplesApi.ts";
 import { SampleData } from "@/types/sampleData.ts";
 
-const AddSamplePage: React.FC = () => {
-  const [sampleData, setSampleData] = useState<SampleData>({
-    name: "",
-    description: "",
-    structure: "",
-    price_per_unit: -1,
-    quantity_unit: "",
-    quantity_left: -1,
-    expiration_date: new Date().toISOString().slice(0, 16),
-    location_id: -1,
-    added_substance_ids: [],
-  });
+const defaultSampleData: SampleData = {
+  name: "",
+  description: "",
+  structure: "",
+  pricePerUnit: 0,
+  quantityUnit: "",
+  quantityLeft: 0,
+  expirationDate: "",
+  locationId: 0,
+  addedSubstanceIds: [],
+};
 
+const AddSamplePage: React.FC = () => {
   const reagentOptions = [
     { id: 1, label: "Reagent 1" },
     { id: 2, label: "Reagent 2" },
@@ -31,92 +31,28 @@ const AddSamplePage: React.FC = () => {
   ];
 
   const [createSample, { isLoading }] = useCreateSampleMutation();
+  const { openSnackbar, SnackbarComponent } = useAlertSnackbar();
 
-  const [openSnackbar, setOpenSnackbar] = useState(false);
-  const [snackbarMessage, setSnackbarMessage] = useState<string | null>(null);
-  const [severity, setSeverity] = useState<"success" | "error">("success");
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setSampleData((prevData) => ({
-      ...prevData,
-      [name]:
-        name === "price_per_unit" || name === "quantity_left"
-          ? Number(value)
-          : value,
-    }));
-  };
-
-  const handleReagentChange = (
-    _event: React.ChangeEvent<unknown>,
-    value: { id: number; label: string }[]
-  ) => {
-    const reagentIdsArray = value.map((reagent) => reagent.id);
-    setSampleData((prevData) => ({
-      ...prevData,
-      added_substance_ids: reagentIdsArray,
-    }));
-  };
-
-  const handleLocationChange = (
-    _event: React.ChangeEvent<unknown>,
-    value: { id: number; label: string } | null
-  ) => {
-    setSampleData((prevData) => ({
-      ...prevData,
-      location_id: value ? value.id : 0,
-    }));
-  };
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
+  const handleSubmit = async (sampleData: SampleData) => {
     try {
       await createSample(sampleData).unwrap();
-      setSnackbarMessage("Sample added successfully!");
-      setSeverity("success");
-      setOpenSnackbar(true);
-
-      setTimeout(() => {
-        setOpenSnackbar(false);
-      }, 3000);
-    } catch (err) {
-      console.error("Error adding sample:", err);
-      setSnackbarMessage("Error adding sample.");
-      setSeverity("error");
-      setOpenSnackbar(true);
+      openSnackbar("success", "Sample created successfully!");
+    } catch (error) {
+      console.error("Failed to create sample:", error);
+      openSnackbar("error", "Failed to create sample.");
     }
-  };
-
-  const handleCloseSnackbar = () => {
-    setOpenSnackbar(false);
   };
 
   return (
     <div>
       <AddSampleForm
-        sampleData={sampleData}
-        handleChange={handleChange}
         handleSubmit={handleSubmit}
-        handleReagentChange={handleReagentChange}
-        handleLocationChange={handleLocationChange}
+        isLoading={isLoading}
         reagentOptions={reagentOptions}
         locationOptions={locationOptions}
+        initialSampleData={defaultSampleData}
       />
-      {isLoading && <p>Loading...</p>}
-
-      <Snackbar
-        open={openSnackbar}
-        autoHideDuration={3000}
-        onClose={handleCloseSnackbar}
-      >
-        <Alert
-          onClose={handleCloseSnackbar}
-          severity={severity}
-          sx={{ width: "100%" }}
-        >
-          {snackbarMessage}
-        </Alert>
-      </Snackbar>
+      <SnackbarComponent />
     </div>
   );
 };
