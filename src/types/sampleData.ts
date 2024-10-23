746282cf58dea76c7221c45dfd185e4a9d7c16a1
Avtex19 <atukaberadze19@gmail.com>
2024-10-23 20:45:11 +0400

    fix: change it from snake_case to camelCase

--- src/types/sampleData.ts
+++ src/types/sampleData.ts
@@ -2,10 +2,10 @@ export type SampleData = {
   name: string;
   description: string;
   structure: string;
-  price_per_unit: number;
-  quantity_unit: string;
-  quantity_left: number;
-  expiration_date: string;
-  location_id: number;
-  added_substance_ids: number[];
+  pricePerUnit: number;
+  quantityUnit: string;
+  quantityLeft: number;
+  expirationDate: string;
+  locationId: number;
+  addedSubstanceIds: number[];
 };
