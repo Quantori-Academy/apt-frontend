b7794c389314bb2d7ee43b36c71f1c4f6fdb8b04
kazrias <ka6rias@gmail.com>
2024-11-04 18:38:34 +0400

    Add orderPage

---
+++ src/pages/OrderPage/OrderPage.tsx
@@ -0,0 +1,19 @@
+// import {
+//   Box,
+//   Divider,
+//   Table,
+//   TableBody,
+//   TableCell,
+//   TableContainer,
+//   TableRow,
+//   Typography,
+// } from "@mui/material";
+
+// import { useTranslation } from "react-i18next";
+// import { useParams } from "react-router-dom";
+
+const OrderPage: React.FC = () => {
+  return <div>OrderPage</div>;
+};
+
+export default OrderPage;
