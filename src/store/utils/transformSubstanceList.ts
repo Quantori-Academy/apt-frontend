5bc7a44ccde39cae2cab20712d2c046acdc1f0a8
kazrias <ka6rias@gmail.com>
2024-11-08 13:23:50 +0400

    Add fallback values for structure and description

--- src/store/utils/transformSubstanceList.ts
+++ src/store/utils/transformSubstanceList.ts
@@ -2,12 +2,13 @@ import { SubstancesDetails, SubstancesResponse } from "@/types";
 
 export const transformSubstanceData = (substances: SubstancesResponse): Array<SubstancesDetails> => {
   return substances.map((substance) => {
+    console.log(substances, "subs");
     return {
       id: substance.id,
       category: substance.category,
-      description: substance.description,
+      description: substance.description || "-",
       name: substance.name,
-      structure: substance.structure,
+      structure: substance.structure || "",
       isExpired: substance.is_expired,
       quantityLeft: `${substance.locations[0].quantity_left} ${substance.locations[0].unit}`,
       storageLocation: `${substance.locations[0].room}/${substance.locations[0].location}`,
