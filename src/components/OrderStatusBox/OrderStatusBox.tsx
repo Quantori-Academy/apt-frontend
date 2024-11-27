171c53af3fdc44da8be65aa15368867f2e4eeffc
kazrias <ka6rias@gmail.com>
2024-11-27 14:13:31 +0400

    feat: add OrderStatusBox component to handle order status changing

---
+++ src/components/OrderStatusBox/OrderStatusBox.tsx
@@ -0,0 +1,62 @@
+import { DoNotDisturb, LocalShipping } from "@mui/icons-material";
+import { Box, Button, Typography } from "@mui/material";
+import { useTranslation } from "react-i18next";
+
+import { ORDER_STATUSES, ORDER_STATUS_COLOR } from "@/constants";
+import { OrderStatus } from "@/types";
+
+type OrderStatusBoxProps = {
+  status: OrderStatus;
+  onClick: () => void;
+};
+
+const OrderStatusBox: React.FC<OrderStatusBoxProps> = ({ status, onClick }) => {
+  const { t } = useTranslation();
+
+  const statusCanBeUpdated =
+    status === ORDER_STATUSES.Pending || status === ORDER_STATUSES.Submitted;
+
+  return (
+    <Box>
+      <Typography variant="subtitle1" color="textSecondary">
+        {t(`orders.table.Status`)}:{" "}
+        <Typography
+          component="span"
+          sx={{
+            color: ORDER_STATUS_COLOR[status],
+            display: "inline",
+          }}
+        >
+          {t(`orders.statuses.${status}`)}
+        </Typography>
+      </Typography>
+
+      {statusCanBeUpdated && (
+        <Button
+          onClick={onClick}
+          sx={{
+            padding: "5px",
+            display: "flex",
+            alignItems: "center",
+            gap: "8px",
+            fontSize: "14px",
+          }}
+        >
+          {status === ORDER_STATUSES.Pending ? (
+            <>
+              <LocalShipping />
+              {t("orders.buttons.markSubmitted")}
+            </>
+          ) : (
+            <>
+              <DoNotDisturb />
+              {t("orders.buttons.markCancelled")}
+            </>
+          )}
+        </Button>
+      )}
+    </Box>
+  );
+};
+
+export default OrderStatusBox;
