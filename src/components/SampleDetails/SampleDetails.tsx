c5882e2e39874dcf9ed23d23a7ce39b33e91d29a
kazrias <ka6rias@gmail.com>
2024-11-29 12:10:06 +0400

    refactor: simplify SampleDetails component by removing quantity and location management, and integrate tabs for displaying added substances and locations

--- src/components/SampleDetails/SampleDetails.tsx
+++ src/components/SampleDetails/SampleDetails.tsx
@@ -1,16 +1,24 @@
-import { Card, CardContent, Grid, Typography } from "@mui/material";
-import React from "react";
+import {
+  Box,
+  Card,
+  CardContent,
+  Grid,
+  Tab,
+  Tabs,
+  Typography,
+} from "@mui/material";
+import { type ReactNode, type SyntheticEvent, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { NavLink } from "react-router-dom";
 
-import { DetailItem, QuantityLocationButtons, SmilesImage } from "@/components";
-import { userRoles } from "@/constants";
-import { useAppSelector } from "@/hooks";
-import { RouteProtectedPath } from "@/router";
-import { selectUserRole } from "@/store";
-import { RoomData, Sample } from "@/types";
+import {
+  AddedSubstancesTable,
+  DetailItem,
+  SmilesImage,
+  SubstanceLocationsTable,
+} from "@/components";
+import { Sample } from "@/types";
 
-type SampleKey = keyof Sample;
+type SampleKey = keyof Omit<Sample, "locations" | "addedSubstances">;
 
 type ReagentDetailRow = {
   label: string;
@@ -19,28 +27,22 @@ type ReagentDetailRow = {
 
 const sampleDetailsRows: ReagentDetailRow[] = [
   { label: "Name", key: "name" },
-  { label: "Substances", key: "addedSubstances" },
   { label: "Quantity left", key: "totalQuantityLeft" },
-  { label: "Storage location", key: "locationId" },
   { label: "Description", key: "description" },
 ];
 
 type SampleDetailsProps = {
   sampleDetails: Sample;
-  sampleLocationDetails: RoomData;
-  setIsChangingQuantity: React.Dispatch<React.SetStateAction<boolean>>;
-  setIsChangingLocation: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-const SampleDetails: React.FC<SampleDetailsProps> = ({
-  sampleDetails,
-  sampleLocationDetails,
-  setIsChangingQuantity,
-  setIsChangingLocation,
-}) => {
+const SampleDetails: React.FC<SampleDetailsProps> = ({ sampleDetails }) => {
   const { t } = useTranslation();
 
-  const role = useAppSelector(selectUserRole);
+  const [value, setValue] = useState(0);
+
+  const handleChange = (_: SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
 
   return (
     <Card sx={{ background: "#0080800f" }}>
@@ -49,58 +51,16 @@ const SampleDetails: React.FC<SampleDetailsProps> = ({
           {t("substanceDetails.title.sample")}
         </Typography>
 
-        <Grid container spacing={2}>
+        <Grid container spacing={2} mb={6}>
           <Grid item xs={12} sm={6}>
             {sampleDetailsRows.map(({ label, key }) => {
-              let value;
-              if (key === "totalQuantityLeft") {
-                value = `${sampleDetails[key]} ${sampleDetails.unit || "-"}`;
-              } else if (key === "locationId") {
-                value = `${sampleLocationDetails.roomName}, ${sampleLocationDetails.locationName}`;
-              } else if (key === "addedSubstances") {
-                value = (
-                  <Typography key={label}>
-                    <Typography
-                      component="span"
-                      fontWeight="bold"
-                      marginRight={1}
-                    >
-                      {t(`substanceDetails.fields.${key}`)}:
-                    </Typography>
-                    {sampleDetails[key].map(({ id, name, category }, index) => (
-                      <React.Fragment key={id}>
-                        <NavLink
-                          to={
-                            category === "Reagent"
-                              ? RouteProtectedPath.reagentPage.replace(
-                                  ":id",
-                                  String(id)
-                                )
-                              : RouteProtectedPath.samplePage.replace(
-                                  ":id",
-                                  String(id)
-                                )
-                          }
-                        >
-                          {name}
-                        </NavLink>
-                        {index < sampleDetails[key].length - 1 && ", "}
-                      </React.Fragment>
-                    ))}
-                  </Typography>
-                );
-              } else {
-                value = sampleDetails[key] || "-";
-              }
-              {
-                return (
-                  <DetailItem
-                    key={label}
-                    label={t(`substanceDetails.fields.${key}`)}
-                    value={value}
-                  />
-                );
-              }
+              return (
+                <DetailItem
+                  key={label}
+                  label={t(`substanceDetails.fields.${key}`)}
+                  value={sampleDetails[key]}
+                />
+              );
             })}
           </Grid>
 
@@ -112,20 +72,53 @@ const SampleDetails: React.FC<SampleDetailsProps> = ({
               <SmilesImage
                 smiles={sampleDetails.structure}
                 svgOptions={{ width: 185, height: 185 }}
+                withBorder
               />
             </Grid>
           )}
         </Grid>
-
-        {role === userRoles.Researcher && (
-          <QuantityLocationButtons
-            onChangeQuantity={() => setIsChangingQuantity(true)}
-            onChangeLocation={() => setIsChangingLocation(true)}
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <Tabs value={value} onChange={handleChange} variant="fullWidth">
+            <Tab label="Added substances" id="tab-1" />
+            <Tab label="Locations" id="tab-2" />
+          </Tabs>
+        </Box>
+        <CustomTabPanel value={value} index={0}>
+          <AddedSubstancesTable
+            addedSubstances={sampleDetails.addedSubstances}
           />
-        )}
+        </CustomTabPanel>
+        <CustomTabPanel value={value} index={1}>
+          <SubstanceLocationsTable
+            locations={sampleDetails.locations}
+            substanceType={sampleDetails.category}
+          />
+        </CustomTabPanel>
       </CardContent>
     </Card>
   );
 };
 
 export default SampleDetails;
+
+type TabPanelProps = {
+  children?: ReactNode;
+  index: number;
+  value: number;
+};
+
+const CustomTabPanel = (props: TabPanelProps) => {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+};
