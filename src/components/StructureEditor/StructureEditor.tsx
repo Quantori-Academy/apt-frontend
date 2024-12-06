430e45bd2b5dc15aa54cb00af0afe9a80e040621
Anahit Margaryan <anahits.codes@gmail.com>
2024-12-06 13:56:01 +0400

    fix editor

--- src/components/StructureEditor/StructureEditor.tsx
+++ src/components/StructureEditor/StructureEditor.tsx
@@ -1,17 +1,17 @@
-import { Ketcher } from "ketcher-core";
+import { Ketcher, RemoteStructServiceProvider } from "ketcher-core";
 import { Editor } from "ketcher-react";
-// @ts-expect-error -- types of ketcher-standalone are outdated
-import { StandaloneStructServiceProvider } from "ketcher-standalone";
 
 import "ketcher-react/dist/index.css";
-
-const structServiceProvider = new StandaloneStructServiceProvider();
 
 type KetcherEditorProps = {
   onChange: (smiles: string) => void;
 };
 
 const StructureEditor: React.FC<KetcherEditorProps> = ({ onChange }) => {
+  const structServiceProvider = new RemoteStructServiceProvider(
+    process.env.REACT_APP_API_PATH!
+  );
+
   const handleInit = (ketcher: Ketcher) => {
     ketcher.editor.event.change.add(async () => {
       const smiles = await ketcher.getSmiles();
--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,6 @@ export default defineConfig({
   },
 
   define: {
-    "process.env": {},
     global: "globalThis",
   },
 });
