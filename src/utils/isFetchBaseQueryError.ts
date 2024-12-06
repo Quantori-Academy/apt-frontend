05885d30dc26d7474958d36bd7a9d7b39a3db210
kazrias <ka6rias@gmail.com>
2024-12-06 07:27:43 +0400

    feat: improve error handling in authSlice using isFetchBaseQueryError utility

--- src/utils/isFetchBaseQueryError.ts
+++ src/utils/isFetchBaseQueryError.ts
@@ -1,4 +1,4 @@
-import { FetchBaseQueryError } from "@reduxjs/toolkit/query/react";
+import type { FetchBaseQueryError } from "@reduxjs/toolkit/query/react";
 
 export const isFetchBaseQueryError = (error: unknown): error is FetchBaseQueryError => {
   return typeof error === "object" && error !== null && "status" in error;
