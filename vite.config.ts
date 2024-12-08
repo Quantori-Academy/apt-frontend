11301528b80627e0e42ebe4d628bfd362dc50886
Nika Gabriadze <67961868+nickgabriadze@users.noreply.github.com>
2024-12-09 00:42:20 +0400

    Update vite.config.ts
    Co-authored-by: DmitriyMischenko-Quantori <dmitriy.mischenko@quantori.com>

--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,6 @@ export default defineConfig({
     },
   },
   build: {
-    commonjsOptions: {transformMixedEsModules: true}
-  }
+    commonjsOptions: { transformMixedEsModules: true },
+  },
 });
