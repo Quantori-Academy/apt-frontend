114c17c6a35d509118ed57351b5037f3ea3bd4e8
Nika Gabriadze <67961868+nickgabriadze@users.noreply.github.com>
2024-12-09 00:42:31 +0400

    Update vite.config.ts
    Co-authored-by: DmitriyMischenko-Quantori <dmitriy.mischenko@quantori.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 /// <reference types="vitest/config" />
 import react from "@vitejs/plugin-react";
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 import tsConfigPaths from "vite-tsconfig-paths";
-import {nodePolyfills} from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
